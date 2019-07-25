a270e1eb7a64631fd835935930775ee1c89e7ff8
Kush Patel <kush.patel@willowtreeapps.com>
2019-07-25 16:55:23 -0400

    idk

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+# from slackclient import SlackClient
+
+app = Flask(__name__)
+# slack_client = SlackClient('SLACK_TOKEN')
+# slack_client.api_call("api.test")
+
+@app.route('/')
+def response():
+    phrase = request.args.get('key')
+    new_phrase = create_mocking_string(phrase)
+    return {'key': new_phrase }
+
+def create_mocking_string(phrase):
+    new_phrase = ""
+    for x, y in enumerate(phrase):
+        new_phrase += alternate_case(x, y)
+    return new_phrase
+
+def alternate_case(x, y):
+    if x % 2 == 0:
+        return y.lower()
+    else:
+        return y.upper()
