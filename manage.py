1bc788aac243d4c0008633be04680efa174f2763
Kush Patel <kush.patel@willowtreeapps.com>
2019-07-30 13:24:59 -0400

    pipenv and .env

--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
 import json
+import os
+import slack
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+client = slack.WebClient(token=slack_token)
 
 app = Flask(__name__)
 
@@ -7,18 +12,32 @@ app = Flask(__name__)
 def response():
     try:
         body = json.loads(request.data)
-        print(body['type'])
+        print(body)
         if body['type'] == 'url_verification':
            return challenge_handler(body)
         elif body['type'] == 'event_callback':
-            return event_handler(body)
+            if body['event']['type'] == 'message' and body['event']['client_msg_id'] != None:
+                return event_handler(body)
         else:
             raise Exception("Error: Incorrect Type")
     except Exception as e:
         return ("error:" + str(e), 400)
 
 def event_handler(body):
-    return create_mocking_string(body["event"]["text"])
+    message = create_mocking_string(body["event"]["text"])
+    client.chat_postMessage(
+     channel="CLR24RLP9",
+     blocks=[
+	    {
+		    "type": "section",
+		    "text": {
+		    	"type": "mrkdwn",
+		    	"text": message
+		    }
+	    }
+    ]
+    )
+    return message
 
 def challenge_handler(body):
     return body["challenge"]
