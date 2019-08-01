9d230561cb14dcfe1af29f2774a803df8841e680
Kush Patel <kush.patel@willowtreeapps.com>
2019-08-01 12:10:05 -0400

    push

--- manage.py
+++ manage.py
@@ -2,42 +2,75 @@ from flask import Flask, request
 import json
 import os
 import slack
+import requests
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 client = slack.WebClient(token=slack_token)
 
 app = Flask(__name__)
+emoji_count = 0
+
+spongebob_mocked_messages = []
 
 @app.route('/', methods=['GET', 'POST'])
 def response():
     try:
         body = json.loads(request.data)
-        print(body)
         if body['type'] == 'url_verification':
            return challenge_handler(body)
         elif body['type'] == 'event_callback':
-            if body['event']['type'] == 'message' and body['event']['client_msg_id'] != None:
-                return event_handler(body)
-        else:
-            raise Exception("Error: Incorrect Type")
+            if body['event']['type'] == "reaction_added":
+                if body['event']['reaction'] == 'grinning':
+                    if body['event']['item']['type'] == "message":
+                        message = getMessage(body)
+                        if message:
+                            return reply_with_bot(message)
+        return ""
     except Exception as e:
         return ("error:" + str(e), 400)
 
-def event_handler(body):
-    message = create_mocking_string(body["event"]["text"])
-    client.chat_postMessage(
-     channel="CLR24RLP9",
-     blocks=[
-	    {
-		    "type": "section",
-		    "text": {
-		    	"type": "mrkdwn",
-		    	"text": message
-		    }
-	    }
-    ]
-    )
-    return message
+def reply_with_bot(message_text):
+    try:
+        message = create_mocking_string(message_text)
+        client.chat_postMessage(
+         channel="CLR24RLP9",
+         blocks=[
+	      {
+		       "type": "section",
+		        "text": {
+		        	"type": "mrkdwn",
+		        	"text": message
+		        }
+	      }
+        ]
+        )
+        return ""
+    except Exception as e:
+        print(e)
+
+
+def getMessage(body):
+    try:
+        channel_history_url = "https://slack.com/api/channels.history"
+        message_channel = body["event"]["item"]["channel"]
+        message_ts = float(body["event"]["item"]["ts"])
+        five_min_messages = message_ts - 30000
+        payload = {
+                'token': slack_token, 
+                'channel': message_channel, 
+                'oldest': five_min_messages
+            }
+        r = requests.get(channel_history_url, params=payload)
+        message_body = r.json()
+
+        for x in message_body["messages"]:
+            if(float(x["ts"]) == message_ts and x["client_msg_id"] not in spongebob_mocked_messages ):
+                spongebob_mocked_messages.append(x["client_msg_id"])                 
+                return x["text"]
+        
+        return False
+    except Exception as e:
+        print(e)
 
 def challenge_handler(body):
     return body["challenge"]
