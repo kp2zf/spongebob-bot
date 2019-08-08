6ead551d8e4b0185e908f0269f7e0b1b9f502c02
Kush Patel <kush.patel@willowtreeapps.com>
2019-08-08 10:56:57 -0400

    redis imple

--- manage.py
+++ manage.py
@@ -1,20 +1,21 @@
+# Kush Patel, Summer 2019 SWE Intern
+
 from flask import Flask, request
-import json
-import os
-import slack
-import requests
+from threading import Thread
+import json, os, slack, requests, redis
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 client = slack.WebClient(token=slack_token)
+red = redis.Redis('localhost')
+message_dict = "message_history"
 
 app = Flask(__name__)
-emoji_count = 0
-
-spongebob_mocked_messages = {}
+message = False
 
 @app.route('/', methods=['GET', 'POST'])
 def response():
     try:
+        print("*** REQUEST RECEIVED ***")
         body = json.loads(request.data)
         if body['type'] == 'url_verification':
            return challenge_handler(body)
@@ -22,6 +23,7 @@ def response():
             if body['event']['type'] == "reaction_added":
                 if body['event']['reaction'] == 'mocking_spongebob':
                     if body['event']['item']['type'] == "message":
+                        initialize_redis()
                         message = getMessage(body)
                         if message:
                             return reply_with_bot(message, body)
@@ -29,9 +31,15 @@ def response():
     except Exception as e:
         return ("error:" + str(e), 400)
 
+def getMessage(body):
+    try:
+        message_body = make_request_for_message_history(body)
+        return is_new_message(body, message_body)
+    except Exception as e:
+        print(e)
+
 def reply_with_bot(message_text, body):
     try:
-        print(body["event"]["item"]["channel"])
         message = create_mocking_string(message_text)
         client.chat_postMessage(
          channel = body["event"]["item"]["channel"],
@@ -49,42 +57,33 @@ def reply_with_bot(message_text, body):
     except Exception as e:
         print(e)
 
-
-def getMessage(body):
-    try:
+def make_request_for_message_history(body):
         channel_history_url = "https://slack.com/api/channels.history"
         message_channel = body["event"]["item"]["channel"]
-        message_ts = float(body["event"]["item"]["ts"])
         current_ts = float(body["event"]["event_ts"])
-        five_min_messages = current_ts - 3600
+        one_hour_messages = current_ts - 3600
         payload = {
                 'token': slack_token, 
                 'channel': message_channel, 
-                'oldest': five_min_messages
+                'oldest': one_hour_messages
             }
         r = requests.get(channel_history_url, params=payload)
         message_body = r.json()
-        print(message_body)
-        for x in message_body["messages"]:
-             if(float(x["ts"]) == message_ts and x["client_msg_id"] not in spongebob_mocked_messages ):
-                spongebob_mocked_messages[x["client_msg_id"]] = "value" 
-                print(x["client_msg_id"])
-                return x["text"]
-            # if((float(x["ts"]) == message_ts):
-            #     if(x["client_msg_id"] not in spongebob_mocked_messages):
-            #         if(x["reactions"] == None):
-            #            spongebob_mocked_messages[x["client_msg_id"]] = "value"
-            #            return x["text"] 
-            
-            # if(x["reactions"] == None or x["reactions"][]):
-            #         spongebob_mocked_messages[x["client_msg_id"]] = "value"
-            #         return x["text"]
+        return message_body
 
-        return False
-    except Exception as e:
-        print(e)
+def is_new_message(body, message_body):
+    message_ts = float(body["event"]["item"]["ts"])
+    for x in message_body["messages"]:
+        if(float(x["ts"]) == message_ts and red.hincrby(message_dict, x["client_msg_id"]) == 1):
+            red.hmset(message_dict, {"test_var": "test"})
+            return x["text"]
+    return False
+
+def initialize_redis():
+    red.hmset(message_dict, {"var": "initialize"})
 
 def challenge_handler(body):
+    print("*** CHALLEGE REQUEST HANDLED***")
     return body["challenge"]
 
 def create_mocking_string(phrase):
